497722138db0144691519fb336d1c2e83977cdfc
Roberto Oliveira <robertooliveira.dev@gmail.com>
2020-01-26 10:39:07 -0300

    Updates

---
+++ precache-manifest.421bcf61fe87d999c2c50d7f0534ab79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c84d7ad3ba55d745b6a74240a2b8e7f",
+    "url": "/english-challenges/index.html"
+  },
+  {
+    "revision": "97d00ec7dc48fa128d61",
+    "url": "/english-challenges/static/css/main.2c03eb7f.chunk.css"
+  },
+  {
+    "revision": "3172bf41f361f8b4b472",
+    "url": "/english-challenges/static/js/2.6163bde8.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/english-challenges/static/js/2.6163bde8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97d00ec7dc48fa128d61",
+    "url": "/english-challenges/static/js/main.e012d4cd.chunk.js"
+  },
+  {
+    "revision": "7d415e21e23543e8cb2c",
+    "url": "/english-challenges/static/js/runtime-main.90e41267.js"
+  }
+]);
