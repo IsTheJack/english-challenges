4e2e5d81cff8da79cfe7f54875f42a124668879b
Roberto Oliveira <robertooliveira.dev@gmail.com>
2020-02-01 02:18:00 -0300

    Updates

---
+++ precache-manifest.5376f05e8124e29a3725cdbb00a909c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cfcb26b3e4a65d21851ec3fb0bf3ce",
+    "url": "/english-challenges/index.html"
+  },
+  {
+    "revision": "3e94fcfefcfe907dacb3",
+    "url": "/english-challenges/static/css/main.037cf0ad.chunk.css"
+  },
+  {
+    "revision": "021d0a8f71d4033639a7",
+    "url": "/english-challenges/static/js/2.1d30820c.chunk.js"
+  },
+  {
+    "revision": "43d47895f184aebd436487b64a300502",
+    "url": "/english-challenges/static/js/2.1d30820c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e94fcfefcfe907dacb3",
+    "url": "/english-challenges/static/js/main.b1ba019e.chunk.js"
+  },
+  {
+    "revision": "7d415e21e23543e8cb2c",
+    "url": "/english-challenges/static/js/runtime-main.90e41267.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/english-challenges/precache-manifest.ca37361e94076c8c071e77f90545cdc8.js"
+  "/english-challenges/precache-manifest.5376f05e8124e29a3725cdbb00a909c8.js"
 );
 
 self.addEventListener('message', (event) => {
